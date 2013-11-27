772e56d37b00df1480404f8d104b7b06b69c1fb9
Tim Holt <tim@astronautweb.co>
2013-11-27 22:40:41 +1100

    updated custom post type functions partial

--- includes/custom-post-type.php
+++ includes/custom-post-type.php
@@ -383,6 +383,45 @@ CPtag
 
 
 
+/**
+ * Remove the slug from published post permalinks. Only affect our CPT though.
+ */
+function ctrl_remove_cpt_slug( $post_link, $post, $leavename ) {
+
+	if( !in_array( $post->post_type, array( 'custompost' ) ) || ( 'publish' != $post->post_status ) ) {
+		return $post_link;	
+	} else {
+		$post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+		return $post_link;
+	}
+	
+}
+// add_filter( 'post_type_link', 'ctrl_remove_cpt_slug', 10, 3 );
+
+
+/**
+ * Some hackery to have WordPress match postname to any of our public post types
+ * All of our public post types can have /post-name/ as the slug, so they better be unique across all posts
+ * Typically core only accounts for posts and pages where the slug is /post-name/
+ */
+function ctrl_parse_request_tricksy( $query ) {
+ 
+    // Only noop the main query
+    if ( ! $query->is_main_query() )
+        return;
+ 
+    // Only noop our very specific rewrite rule match
+    if ( 2 != count( $query->query )
+        || ! isset( $query->query['page'] ) )
+        return;
+ 
+    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
+    if ( ! empty( $query->query['name'] ) )
+        $query->set( 'post_type', array( 'post', 'custompost', 'page' ) );
+}
+// add_action( 'pre_get_posts', 'ctrl_parse_request_tricksy' );
+
+
 
 //--end CPT plugin
 
