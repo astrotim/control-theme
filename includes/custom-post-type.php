c2f538ddeac6cac8ecad55428188448d9df4c108
Tim Holt <tim@astronautweb.co>
2014-05-01 13:40:41 +1000

    updated custom post type template

--- includes/custom-post-type.php
+++ includes/custom-post-type.php
@@ -1,10 +1,10 @@
 <?php
-/*  
+/*
 Plugin Name: Custom Post Type base
-Description: Custom post type plugin 
-Version: 1.0  
-Author: Tim Holt  
-*/  
+Description: Custom post type plugin
+Version: 1.0
+Author: Tim Holt
+*/
 
 /*
 Contents:
@@ -25,14 +25,14 @@ Contents:
 checklist for deployment
 
 ****** find & replace labels, meta field names, etc
-custompost				
-CustomPost				
-cptcategory				
-CPT Categories			
-CPT Category			
-cptcat					
-cptag					
-CPtag					
+custompost
+CustomPost
+cptcategory
+CPT Categories
+CPT Category
+cptcat
+cptag
+CPtag
 
 */
 
@@ -49,7 +49,7 @@ CPtag
 			'view_item' => __('View CustomPost Page'),
 			'search_items' => __('Search CustomPosts'),
 			'not_found' =>  __('No custompost found'),
-			'not_found_in_trash' => __('No custompost found in Trash'), 
+			'not_found_in_trash' => __('No custompost found in Trash'),
 			'parent_item_colon' => ''
 		);
 		$args = array (
@@ -58,7 +58,7 @@ CPtag
 			'show_ui' => true,
 			'publicly_queryable' => true,
 			'exclude_from_search' => false,
-			'_builtin' => false, 
+			'_builtin' => false,
 			'_edit_link' => 'post.php?post=%d',
 			'capability_type' => 'post',
 			'hierarchical' => false,
@@ -68,10 +68,10 @@ CPtag
 			'menu_position' => 20,
 			'supports' => array('title' , 'editor', 'thumbnail', 'revisions'),
 		);
-	register_post_type( 'custompost', $args); 
-	
+	register_post_type( 'custompost', $args);
+
 }//--end create_post_type_custompost
-	
+
 	//--hook CPT to init
 	add_action('init', 'create_post_type_custompost');
 
@@ -87,7 +87,7 @@ CPtag
 
 //	2. taxonomies
 	function create_custompost_taxonomies() {
-		//	2a. add taxonomy (hierarchical for category format)		
+		//	2a. add taxonomy (hierarchical for category format)
 		$labels = array(
 			'name' => __( 'CPT Categories', 'taxonomy general name' ),
 			'singular_name' => __( 'CPT Category', 'taxonomy singular name' ),
@@ -99,16 +99,16 @@ CPtag
 			'update_item' => __( 'Update CPT Category' ),
 			'add_new_item' => __( 'Add New CPT Category' ),
 			'new_item_name' => __( 'New CPT Category' ),
-		); 	
+		);
 		$args = array (
 			'labels' => $labels, /* array from above */
 			'hierarchical' => true,
 			'show_ui' => true,
 			'query_var' => true,
-			'rewrite' => array( 'slug' => 'cptcategory' ),			
+			'rewrite' => array( 'slug' => 'cptcategory' ),
 		);
 		register_taxonomy( 'cptcategory', 'custompost', $args ); /* must have CPT as 2nd argument */
-		
+
 		//	2b. add another taxonomy (non-hierarchical for tag format)
 		$labels = array(
 			'name' => _x( 'CPTags', 'taxonomy general name' ),
@@ -118,7 +118,7 @@ CPtag
 			'all_items' => __( 'All CPTags' ),
 			'parent_item' => null,
 			'parent_item_colon' => null,
-			'edit_item' => __( 'Edit CPTag' ), 
+			'edit_item' => __( 'Edit CPTag' ),
 			'update_item' => __( 'Update CPTag' ),
 			'add_new_item' => __( 'Add New CPTag' ),
 			'new_item_name' => __( 'New CPTag Name' ),
@@ -135,19 +135,19 @@ CPtag
 			'rewrite' => array( 'slug' => 'cptag' ),
 		);
 		register_taxonomy( 'cpttag', 'custompost', $args ); /* must have CPT as 2nd argument */
-		
+
 	}//--end create_custompost_taxonomies
-	
+
 	// hook taxonomies to init
 	add_action( 'init', 'create_custompost_taxonomies', 0 );
 
 
 //	3. create meta options box
 	function meta_options_custompost() {
-		
+
 	// Use nonce for verification
 	wp_nonce_field( plugin_basename( __FILE__ ), 'custompost_noncename' );
-		
+
 		global $post;
 		$custom = get_post_custom($post->ID);
 			  $meta_field_1 = $custom["_meta_field_1"][0];
@@ -190,19 +190,19 @@ CPtag
 
 //	5. save post
 	function save_details_custompost(){
-		
-	// verify if this is an auto save routine. 
+
+	// verify if this is an auto save routine.
 	// If it is our form has not been submitted, so we dont want to do anything
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
 		return;
-	
+
 	// verify this came from the our screen and with proper authorization,
 	// because save_post can be triggered at other times
 	if ( !wp_verify_nonce( $_POST['custompost_noncename'], plugin_basename( __FILE__ ) ) )
 		return;
-	
+
 	// Check permissions
-	if ( 'page' == $_POST['post_type'] ) 
+	if ( 'page' == $_POST['post_type'] )
 	{
 	  if ( !current_user_can( 'edit_page', $post_id ) )
 		  return;
@@ -212,15 +212,15 @@ CPtag
 	  if ( !current_user_can( 'edit_post', $post_id ) )
 		  return;
 	}
-		
+
 	global $post;
 		update_post_meta($post->ID, "_meta_field_1", $_POST["_meta_field_1"]);
 		update_post_meta($post->ID, "_meta_field_2", $_POST["_meta_field_2"]);
 		update_post_meta($post->ID, "_meta_field_3", $_POST["_meta_field_3"]);
 		update_post_meta($post->ID, "_meta_field_4", $_POST["_meta_field_4"]);
-		
+
 	}//--end save_details_custompost
-	
+
 	//hook save post
 	add_action('save_post', 'save_details_custompost');
 
@@ -230,14 +230,14 @@ CPtag
 //	6. change default title prompt
 	function change_custompost_title( $title ){
 		 $screen = get_current_screen();
-	 
+
 		 if  ( 'custompost' == $screen->post_type ) {
 			  $title = 'Enter CustomPost Name';
 		 }
-	 
+
 		 return $title;
 	}
-	
+
 	// hook filter
 	add_filter( 'enter_title_here', 'change_custompost_title' );
 
@@ -286,7 +286,7 @@ CPtag
 	}//--end custom_columns
 
 	//	hook columns
-	add_filter("manage_edit-custompost_columns", "edit_columns"); 
+	add_filter("manage_edit-custompost_columns", "edit_columns");
 	add_action("manage_custompost_posts_custom_column", "custom_columns", 10, 2); // need the 10,2 here for columns output to work (2 of 2)
 
 
@@ -295,18 +295,18 @@ CPtag
 //	8. taxonomy dropdown filter
 	function taxonomy_filter_restrict_manage_posts() { //build drop down
 		global $typenow, $wp_query;
-	
+
 		$post_types = get_post_types( array( '_builtin' => false ) );
-	
+
 		if ( in_array( $typenow, $post_types ) ) {
 			$filters = get_object_taxonomies( $typenow );
-	
+
 			foreach ( $filters as $tax_slug ) {
 				$tax_obj = get_taxonomy( $tax_slug );
 
 				// check if anything has been selected, else set selected to null
 				$selected = isset($wp_query->query[$tax_slug]) ? $wp_query->query[$tax_slug] : null;
-				
+
 				wp_dropdown_categories( array(
 					'show_option_all'	=> __('Show All ' . $tax_obj->label . '&nbsp;'),
 					'taxonomy' 	  		=> $tax_slug,
@@ -326,11 +326,11 @@ CPtag
 	}
 	add_action( 'restrict_manage_posts', 'taxonomy_filter_restrict_manage_posts' );
 
-	
+
 	function taxonomy_filter_post_type_request( $query ) { //add filter to query so dropdown will work
 		global $pagenow, $typenow;
 
-		if ( 'edit.php' != $pagenow ) 
+		if ( 'edit.php' != $pagenow )
 			return;
 
 		$filters = get_object_taxonomies( $typenow );
@@ -354,7 +354,7 @@ CPtag
 		// 		print_r($term);
 		// 		echo '</pre>';
 		// 		$slug = $term->slug;
-		// 		echo '[slug]: ' . $slug . '<br><br>';	
+		// 		echo '[slug]: ' . $slug . '<br><br>';
 		// 	} else {
 		// 		echo 'no term selected<br><br>';
 		// 	}
@@ -386,17 +386,17 @@ CPtag
 /**
  * Remove the slug from published post permalinks. Only affect our CPT though.
  */
-function ctrl_remove_cpt_slug( $post_link, $post, $leavename ) {
+	function ctrl_remove_cpt_slug( $post_link, $post, $leavename ) {
 
-	if( !in_array( $post->post_type, array( 'custompost' ) ) || ( 'publish' != $post->post_status ) ) {
-		return $post_link;	
-	} else {
-		$post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
-		return $post_link;
+		if( !in_array( $post->post_type, array( 'custompost' ) ) || ( 'publish' != $post->post_status ) ) {
+			return $post_link;
+		} else {
+			$post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+			return $post_link;
+		}
+
 	}
-	
-}
-// add_filter( 'post_type_link', 'ctrl_remove_cpt_slug', 10, 3 );
+	// add_filter( 'post_type_link', 'ctrl_remove_cpt_slug', 10, 3 );
 
 
 /**
@@ -404,28 +404,104 @@ function ctrl_remove_cpt_slug( $post_link, $post, $leavename ) {
  * All of our public post types can have /post-name/ as the slug, so they better be unique across all posts
  * Typically core only accounts for posts and pages where the slug is /post-name/
  */
-function ctrl_parse_request_tricksy( $query ) {
- 
-    // Only noop the main query
-    if ( ! $query->is_main_query() )
-        return;
- 
-    // Only noop our very specific rewrite rule match
-    if ( 2 != count( $query->query )
-        || ! isset( $query->query['page'] ) )
-        return;
- 
-    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
-    if ( ! empty( $query->query['name'] ) )
-        $query->set( 'post_type', array( 'post', 'custompost', 'page' ) );
-}
-// add_action( 'pre_get_posts', 'ctrl_parse_request_tricksy' );
+	function ctrl_parse_request_tricksy( $query ) {
+
+	    // Only noop the main query
+	    if ( ! $query->is_main_query() )
+	        return;
+
+	    // Only noop our very specific rewrite rule match
+	    if ( 2 != count( $query->query )
+	        || ! isset( $query->query['page'] ) )
+	        return;
+
+	    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
+	    if ( ! empty( $query->query['name'] ) )
+	        $query->set( 'post_type', array( 'post', 'custompost', 'page' ) );
+	}
+	// add_action( 'pre_get_posts', 'ctrl_parse_request_tricksy' );
+
+
+
+// admin columns
+	function custompost_admin_columns() {
+		$columns = array(
+			'cb'	 	=> '<input type="checkbox" />',
+			'image' 	=> '',
+			'acf_number'=> 'No.',
+			'title' 	=> 'Title',
+			'acf_field'	=> 'Custom Field',
+
+		);
+		return $columns;
+	}
+	// add_filter("manage_custompost_posts_columns", "custompost_admin_columns");
+
+
+	function custompost_custom_columns($column, $post_id) {
+		global $post;
+		switch ($column) {
+			case "acf_number":
+				echo get_field('acf_number', $post_id);
+				break;
+			case "image":
+				$image_src 	= get_field('cover_art'); // or post_thumbnail
+				if($image_src) {
+					$thumb_src = $image_src['sizes']['thumbnail'];
+					echo '<img src="' . $thumb_src . '" width="40" height="40">';
+				}
+				break;
+			case "acf_field":
+				echo get_field('acf_field', $post_id);
+				break;
+		}
+	}
+	// add_action("manage_custompost_posts_custom_column", "custompost_custom_columns", 10, 2);
+
+
+
+	function cpt_custom_column_width() {
+		?>
+		<style>
+			#posts-filter th#column_name {
+				width: 65px;
+			}
+		</style>
+		<?php
+	}
+	// add_action("admin_head", "cpt_custom_column_width");
+
+
+	function remove_row_actions_links($actions, $post) {
+		if ($post->post_type == "cpt"){
+			unset( $actions['inline hide-if-no-js'] ); // quick edit
+			unset( $actions['trash'] );
+			unset( $actions['view'] );
+		}
+
+	   return $actions;
+	}
+	// add_filter('post_row_actions', 'remove_row_actions_links', 10, 2);
+
+
+	function set_cpt_admin_order($query) {
+		if (is_admin()) {
+
+			// Get the post type from the query
+			$post_type = $query->query['post_type'];
+
+			if ( $post_type == 'song') {
+				$query->set('orderby', 'meta_value_num');
+				$query->set('meta_key', 'number');
+				$query->set('order', 'ASC');
+			}
+		}
+	}
+	// add_filter('pre_get_posts', 'set_cpt_admin_order');
 
 
 
 //--end CPT plugin
-
-
 
 
 ?>
